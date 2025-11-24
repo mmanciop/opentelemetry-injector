8c53eceb957f989dfd2dea05ec83fc3f05bcd731
Antoine Toulme <atoulme@splunk.com>
2025-11-24 05:43:04 -0800

    [chore] test both amd64 and arm64 archs (#131)
    * [chore] test both archs
    
    * use a base image with .NET 9 installed
    
    * add debug
    
    * more fixes
    
    * wip
    
    * Exclude dotnet from running arm64

--- packaging/tests/rpm/nodejs/run.sh
+++ packaging/tests/rpm/nodejs/run.sh
@@ -5,8 +5,10 @@ set -euo pipefail
 arch="${ARCH:-amd64}"
 if [ "$arch" = arm64 ]; then
   docker_platform=linux/arm64
+  rpm_arch=aarch64
 elif [ "$arch" = amd64 ]; then
   docker_platform=linux/amd64
+  rpm_arch=x86_64
 else
   echo "The architecture $arch is not supported."
   exit 1
@@ -18,6 +20,7 @@ cd "$SCRIPT_DIR/../../../.."
 docker build \
   --platform "$docker_platform" \
   --build-arg "ARCH=$arch" \
+  --build-arg "RPM_ARCH=$rpm_arch" \
   -t "instrumentation-nodejs-$arch" \
   -f packaging/tests/rpm/nodejs/Dockerfile \
   .
