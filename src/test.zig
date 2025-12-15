487685fc270f8366d19f700ec86b3c2d5f42a053
Bastian Krol <bastian.krol@dash0.com>
2025-12-15 07:23:29 +0100

    test: add missing import to test.zig, remove unused imports (#161)

--- src/test.zig
+++ src/test.zig
@@ -2,17 +2,15 @@
 // SPDX-License-Identifier: Apache-2.0
 
 // All files with unit tests need to be referenced here:
-pub const alloc = @import("allocator.zig");
+pub const arg_parser = @import("args_parser.zig");
 pub const config = @import("config.zig");
 pub const dotnet = @import("dotnet.zig");
 pub const jvm = @import("jvm.zig");
 pub const node_js = @import("node_js.zig");
-pub const print = @import("print.zig");
-pub const print_test = @import("print_test.zig");
-pub const res_attrs = @import("resource_attributes.zig");
+pub const patterns_matcher = @import("patterns_matcher.zig");
+pub const patterns_util = @import("patterns_util.zig");
+pub const print_test_ = @import("print_test.zig");
 pub const res_attrs_test = @import("resource_attributes_test.zig");
-pub const root = @import("root.zig");
-pub const types = @import("types.zig");
 
 // Provide a C-style `char **environ` variable to the linker, to satisfy the
 //   extern var __environ: [*]u8;
